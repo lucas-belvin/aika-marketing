6a5e395368a02cac60632aed41751c39b2ec15c0
Lucas Swartsenburg <lucas@itso.io>
2019-11-13 13:53:52 -0600

    rm

--- src/templates/landing.js
+++ src/templates/landing.js
@@ -19,7 +19,6 @@ class Landing extends React.Component {
             AOS.init({
                 offset: -100,
                 duration: 1500,
-                disable: 'mobile'
             });
         }
     };
